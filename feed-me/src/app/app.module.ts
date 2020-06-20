a419e9d6a1d67d1fb2162e7b9cfaadd21ab39515
Emma Coetzer <emmac@bbd.co.za>
2020-06-20 13:44:24 +0200

    feat: set up folder structure

--- feed-me/src/app/app.module.ts
+++ feed-me/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LoginComponent } from './modules/login/login.component';
+import { SignupComponent } from './modules/signup/signup.component';
+import { SideNavComponent } from './shared/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
