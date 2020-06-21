4fe70b8d42785a651b77766fbca9e69b2f9404ba
Emma Coetzer <emmac@bbd.co.za>
2020-06-21 17:58:32 +0200

    feat: added environments to gitignore

---
+++ front-end/feed-me/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC0NhMfNpKbPGTz1cok0b_zz6rWTtdWffA",
+    authDomain: "feedme-208a2.firebaseapp.com",
+    databaseURL: "https://feedme-208a2.firebaseio.com",
+    projectId: "feedme-208a2",
+    storageBucket: "feedme-208a2.appspot.com",
+    messagingSenderId: "2473855286",
+    appId: "1:2473855286:web:da00e89eabb5c5624a8b97",
+    measurementId: "G-D46M7L07MZ"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
